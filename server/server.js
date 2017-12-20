169b1b5375fde4344193b3b776c5270ba0788d9b
WhizzMirray <ruffneck@dr.com>
2017-12-20 11:10:29 +0100

    Procfile

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const PORT = 3000;
+const PORT = process.env.PORT || 5000
 
 /* == initialization == */
 const app = require('./app');
