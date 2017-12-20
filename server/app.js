4b3f9173fcfe3fc82c732c3510ad92fc1e273f99
WhizzMirray <ruffneck@dr.com>
2017-12-20 10:59:49 +0100

    morgan logger

--- server/app.js
+++ server/app.js
@@ -5,7 +5,11 @@ const session = require('express-session');
 const passport = require('passport');
 const pgSession = require('connect-pg-simple')(session);
 const db = require('./queries');
+var morgan = require('morgan');
 const app = express();
+
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 app.use( (req, res, next ) => {
   res.header("Access-Control-Allow-Origin", "*");
